c6d18d2ce86ae7341917d4cc4a7e9cd3f30eb6dc
benoitgraziani <benoit.c.graziani@hotmail.fr>
2021-10-07 15:22:17 +0200

    e

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,5 +4,11 @@ class PagesController < ApplicationController
     def about
     end
     def contact
+        @members = ["diane", "jean", "boris", "bertrand"]
+        if params[:member]
+            @members = @members.select { |member| member.start_with?(params[:member])}
+        end
     end
 end
+
+
